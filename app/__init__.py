33c5d84cae7cf364e716bdaa5c41bcdf91dcab56
freestyletear <freestyletear@gmail.com>
2021-09-26 16:25:02 +0300

    create template boilerplates, landing page and modify it's code

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'kj29'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
